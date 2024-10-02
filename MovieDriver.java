d9d345bd4ecdd6720d6c46ed905c3bf30ac653ae
IvanDmyt <agent.id0503@gmail.com>
2024-10-02 18:37:30 -0400

    Add files via upload

--- MovieDriver.java
+++ MovieDriver.java
@@ -7,47 +7,36 @@
  * I pledge that I have completed the programming assignment 
 * independently. I have not copied the code from a student or   * any source. I have not given my code to any student.
  * Print your Name here: Ivan Sergiyovych Dmytriiev
-*/
+*/  
 import java.util.Scanner;
 
 public class MovieDriver {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        boolean i = true;
-        
-        while (i) {
-            Movie movie = new Movie();
+        Movie movie = new Movie();
             
             
-            System.out.print("Enter the name of a movie ");
-            String title = scanner.nextLine();
-            movie.setTitle(title);  
+        System.out.print("Enter the name of a movie ");
+        String title = scanner.nextLine();
+        movie.setTitle(title);  
             
            
-            System.out.print("Enter the rating of the movie ");
-            String rating = scanner.nextLine();
-            movie.setRating(rating);  
+        System.out.print("Enter the rating of the movie ");
+        String rating = scanner.nextLine();
+        movie.setRating(rating);  
             
           
-            System.out.print("Enter the number of tickets sold for this movie ");
-            int ticketsSold = scanner.nextInt();
-            movie.setSoldTickets(ticketsSold);  
+        System.out.print("Enter the number of tickets sold for this movie ");
+        int soldTickets = scanner.nextInt();
+        movie.setSoldTickets(soldTickets);  
             
-            scanner.nextLine();  
+        scanner.nextLine();  
             
-            System.out.println(movie.toString());
-            
-            System.out.print("\nDo you want to enter another? (y or n) ");
-            String userResponse = scanner.nextLine();
-            
-            if (!userResponse.equalsIgnoreCase("y")) {
-                i = false;
-                System.out.println("Goodbye");
-            }
-        }
+        System.out.println(movie.toString());
+
+        System.out.println("Goodbye");
         
         scanner.close();
+       }
     }
-}
